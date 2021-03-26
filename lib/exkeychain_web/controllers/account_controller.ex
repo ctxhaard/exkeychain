82bcbd7509484db2f888c85930c47c77061ba235
Carlo Tomasin <carlo.tomasin@icare-world.com>
2021-03-26 20:25:49 +0100

    bugfix

--- lib/exkeychain_web/controllers/account_controller.ex
+++ lib/exkeychain_web/controllers/account_controller.ex
@@ -1,45 +1,45 @@
 defmodule ExkeychainWeb.AccountController do
   use ExkeychainWeb, :controller
-  
+
   require Logger
-  
+
   def entrypoint(conn, _params) do
     if :kc_server.is_loaded() do
       redirect(conn, to: "/accounts/")
     else
       redirect(conn, to: Routes.account_path(conn, :load) )
     end
-    
+
   end
-  
+
   def form(conn, _params) do
     render(conn, "load.html")
   end
-  
+
   def load(conn, %{ "file" => file, "pwd" => pwd } = params) do
     Logger.info("params: #{ inspect params }")
     with :ok <- :kc_server.load( file, pwd),
     #do: redirect(conn, to: Routes.account_path(conn, :index) )
     do: render(conn, :authentication)
   end
-  
+
   def index(conn, _params) do
     accounts = get_accounts([])
     render(conn, :index, accounts: accounts )
   end
-  
+
   def show(conn, %{ "id" => id }) do
     {:account, a} = id |> String.to_integer() |> :kc_server.get()
     render(conn, :show, account: a)
   end
-  
+
   def new(conn, _params) do
     # used to get a new, unsaved, account to edit and then
     # submit to creare
-    a = :kc_server.new_account()
+    {:account, a} = :kc_server.new_account()
     render(conn, :show, account: a)
   end
-  
+
   def create(conn, params) do
     # used to submit a newly created account
     Logger.info("#{ __MODULE__ }#{  elem(__ENV__.function,0) }")
@@ -48,7 +48,7 @@ defmodule ExkeychainWeb.AccountController do
     :kc_server.put(a)
     render(conn, :update, account: a)
   end
-  
+
   def update(conn, params) do
     Logger.info("#{ __MODULE__ }#{  elem(__ENV__.function,0) }")
     convertedParams = for { key, val} <- params, into: %{}, do: {String.to_atom(key), val}
@@ -56,13 +56,13 @@ defmodule ExkeychainWeb.AccountController do
     :kc_server.put(a)
     render(conn, :update, account: a)
   end
-  
+
   def delete(conn, %{ "id" => id }) do
     id |> String.to_integer() |> :kc_server.delete()
     Logger.info("#{ __MODULE__ }#{  elem(__ENV__.function,0) }")
     render(conn, :delete, id: id)
   end
-  
+
   @type account :: [%{}]
   @spec get_accounts([account] | []) :: [account | []]
   defp get_accounts([]) do
@@ -71,7 +71,7 @@ defmodule ExkeychainWeb.AccountController do
       _ -> []
     end
   end
-  
+
   defp get_accounts(acc) do
     case :kc_server.next() do
       {:account, a} -> get_accounts([a|acc])
